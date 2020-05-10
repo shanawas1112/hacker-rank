38c2e16c4537edb736affb5e5e0087083b98850f
shanawas1112 <42438666+shanawas1112@users.noreply.github.com>
2020-05-10 10:47:40 +0530

    Update If_else.py

--- If_else.py
+++ If_else.py
@@ -1,3 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
 n = int(input())
-for i in range(1,n+1):
-        print(i,end="")
+
+listOfAnswers = [[i, j, k] 
+for i in range(a + 1) 
+  for j in range(b + 1) 
+     for k in range(c + 1) 
+if i + j + c != n] 
+print(listOfAnswers)
