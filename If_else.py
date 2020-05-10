c0abd9bae84f3e9e05e711b794a5ba6c398e075e
shanawas1112 <42438666+shanawas1112@users.noreply.github.com>
2020-05-10 10:41:58 +0530

    Update If_else.py

--- If_else.py
+++ If_else.py
@@ -1,9 +1,5 @@
-n=int(input())
-if n%2==1:
-    print("weird")
-elif n%2==0 and (n>=2 and n<5):
-    print("not weird")
-elif n%2==0 and (n>=6 and n<=20):
-    print(" weird")
-elif n%2==0 and (n>20):
-    print("not weird")
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b) 
